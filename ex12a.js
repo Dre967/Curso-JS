9d8f64acb317514fdebdcf2905b335fd506df53b
André Venâncio dos Santos <64700803+Dre967@users.noreply.github.com>
2020-08-30 18:22:26 -0400

    tudo feito hoje

---
+++ ex12a.js
@@ -0,0 +1,11 @@
+var hora = 24
+var Agora = new Date()
+var hora = Agora.getHours
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom dia!')
+} else if (hora <=18) {
+    console.log('Boa tarde')
+} else {
+    console.log('Boa noite')
+}
